736b95c8b5d892d982fcc1a3045490d437dd01ee
Pavel <pavelcorolenko535@gmail.com>
2021-10-10 16:37:46 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-tgAPI = '1971672977:AAElchngswZKSaXA9Dza_WgISjoNO66q7RI'
-binanceAPI = {
-        'api_key':'Jvb96aqeAu0NVyCyDRWwraZOtFgGAU90D88kD8Wg9VKmVi7GxNvc3nPFLy3fEl5Z',
-        'secret_key':'tvAcpJTlN82e4OLg2sRkTAVWw1YREcTX8yeZgxkZO414nU4E0NNewo8Noalq4Gr5',
-    }
